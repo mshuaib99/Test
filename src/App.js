c86dd434d8bf65c82808a9793590e293fab6354b
Mahdy Shuaib <mahdy.shuaib99@gmail.com>
2025-02-12 05:30:59 -0600

    Create website template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { useState } from "react";
+import Template from "./template/Template";
+import ProductDetail from "./products/detail/ProductDetail";
+import { Routes , Route} from 'react-router-dom';
+import Landing from "./landing/Landing";
+import ProductList from "./products/ProductList";
 
-export default function App() {
-  const [count, setCount] = useState(0);
-
+function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
-      <h1 className="text-3xl font-bold mb-4">Welcome to My React App</h1>
-      <p className="text-lg">This site is deployed on GitHub Pages with a custom domain!</p>
-      <div className="mt-4">
-        <button
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
-          onClick={() => setCount(count + 1)}
-        >
-          Click Me ({count})
-        </button>
-      </div>
-    </div>
+    <Template>
+      <Routes>
+        <Route path="/products" element={<ProductList />}></Route>
+        <Route path="/products/:slug" element={<ProductDetail />}></Route>
+        <Route path="/" element={<Landing />}></Route>
+      </Routes>
+    </Template>
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import "./bootstrap-custom.css";
+import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { BrowserRouter,  Routes , Route  } from "react-router-dom";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+library.add(fas, far, fab);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App /> }>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
